45a0da57278635092c03415a642a8661e43b54ad
SimonG <saguti15@g.holycross.edu>
2025-07-20 18:31:34 -0400

    added credit genie

---
+++ CreditGenie.js
@@ -0,0 +1,72 @@
+/*
+PROMPT:
+    - Create a shopping cart that has the folloing methods
+        * Add Item
+        * Remove Item
+        * Get Total
+        * Apply Coupons
+*/
+
+let shoppingCart = new Map();
+let couponBook = new Map();
+
+function addItem(name, price) {
+    if (shoppingCart.has(name)) {
+        const item = shoppingCart.get(name);
+        shoppingCart.set(name, {quantity: item.quantity + 1, price: item.price});
+    } else {
+        shoppingCart.set(name, {quantity: 1, price});
+    }
+}
+
+function removeItem(name) {
+    if (!shoppingCart.has(name)) {
+        console.log('Item does not exist!');
+        return;
+    }
+
+    const item = shoppingCart.get(name);
+
+    if (item.quantity === 1) {
+        shoppingCart.delete(name);
+        console.log(`All ${name}s are removed!`);
+    } else {
+        shoppingCart.set(name, {quantity: item.quantity - 1, price: item.price });
+        console.log(`A ${name} was removed!`);
+    }
+}
+
+function applyCoupon(name, coupon) {
+    couponBook.set(name, coupon);
+}
+
+function getTotal(cart, book){
+    let total = 0;
+
+    for (const [name, item] of cart) {
+        total += (item.quantity * Number(item.price)) * (book.get(name) || 1);
+    }
+
+    console.log(total);
+    return total;
+}
+
+
+addItem('apple', '2.50');
+addItem('pear', '3.50');
+addItem('apple', '2.50');
+console.log(shoppingCart);
+
+removeItem('apple');
+console.log(shoppingCart);
+
+removeItem('pear');
+console.log(shoppingCart);
+
+removeItem('bannana');
+console.log(shoppingCart);
+
+applyCoupon('apple', 0.50);
+
+getTotal(shoppingCart, couponBook);
+
